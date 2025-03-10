9f15816bde5f9e54cd28b41c7cdc13c72dd65145
Diego De la Vega <diego.delavega@crehana.com>
2025-03-10 17:23:17 -0500

    Changes

--- app/evento/page.tsx
+++ app/evento/page.tsx
@@ -1,6 +1,5 @@
 
 import Invitation from "@/app/components/Invitation/invitation";
-import QRScanner from "@/app/components/Invitation/scan-invitation";
 export default function Invitations() {
     return (
         <main>
