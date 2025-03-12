3f1ee04f6cf721760b4a15ac3f14370c816f06b4
malsebasmal <antoniomaldonadoestudio@gmail.com>
2025-03-12 17:57:35 -0500

    add: new files for the structure

---
+++ app/new-landing/sections/Hero/Hero.tsx
@@ -0,0 +1,21 @@
+import Bounce from "@/app/new-landing/sections/Hero/components/Bounce"
+import ButtonLink from "@/app/new-landing/components/ButtonLink"
+
+
+const Hero = () => {
+  return (
+    <section className="bg-black text-white w-full h-dvh flex flex-col justify-center items-center">
+      <div className="flex flex-col justify-center items-center gap-4">
+        <h2 className="font-bold text-2xl">HAZ CRECER TU PATRIMONIO Y</h2>
+        <h1 className="text-center text-8xl">Logra tus metas <br /> financieras</h1>
+        <h2 className="text-2xl text-gray-500">Somos Finniu una empresa gestora de fondos privados</h2>
+        <ButtonLink href="#" text="Proyecta tus Metas" className=" mt-4 text-white py-4 px-6 rounded-full bg-purpleSecondary"/>
+      </div>
+      <div className="mt-20">
+        <Bounce />
+      </div>
+    </section>
+  )
+}
+
+export default Hero
---
+++ app/new-landing/sections/Hero/components/Bounce.tsx
@@ -0,0 +1,12 @@
+import Arrowdown from "@/app/new-landing/sections/Hero/icons/Arrowdown"
+
+const Bounce = () => {
+  return (
+    <button className="flex flex-col items-center gap-2">
+      Desliza hacia abajo
+      <Arrowdown />
+    </button>
+  )
+}
+
+export default Bounce
