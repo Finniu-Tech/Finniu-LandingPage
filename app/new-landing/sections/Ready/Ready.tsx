37174c9490b1c3f94e4631935925033eb637eeb2
malsebasmal <antoniomaldonadoestudio@gmail.com>
2025-03-13 15:07:04 -0500

    add: section ready but missing the style of macbook

---
+++ app/new-landing/sections/Ready/Ready.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image"
+import ButtonLink from "@/app/new-landing/components/ButtonLink"
+import Mackbook from "@/app/new-landing/sections/Ready/images/Macbook.webp"
+
+const Ready = () => {
+  return (
+    <section className="py-40 bg-white text-black flex justify-center items-center gap-12">
+      <div className="flex flex-col justify-center items-start gap-8">
+        <h3 className="font-bold text-base">LISTOS PARA CRECER CONTIGO</h3>
+        <h3 className="w-[30rem] text-6xl leading-tight">Sin requisitos ocultos y sin comisiones</h3>
+        <h3 className="w-[30rem]">Regístrate de cualquier celular o computador y obtén más ganancias con tus ahorros. Genera un ingreso extra al mes</h3>
+        <ButtonLink href="#" text="Empieza ahora" className="mt-4 text-white py-4 px-6 rounded-full bg-purplePrincipal" />
+      </div>
+      <div className="w-[50rem]">
+        <Image src={Mackbook} alt="Mackbook Finniu" className="w-full"/>
+      </div>
+    </section>
+  )
+}
+
+export default Ready
