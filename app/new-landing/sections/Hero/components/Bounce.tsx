69ffaaae4c1c66c43865f991ddfaf03cbae43bd6
malsebasmal <antoniomaldonadoestudio@gmail.com>
2025-03-17 11:52:31 -0500

    change: animation bounce in hero and background for sections invest | add: ; for use client

--- app/new-landing/sections/Hero/components/Bounce.tsx
+++ app/new-landing/sections/Hero/components/Bounce.tsx
@@ -2,9 +2,9 @@ import Arrowdown from "@/app/new-landing/sections/Hero/icons/Arrowdown"
 
 const Bounce = () => {
   return (
-    <button className="text-lg text-gray-400 flex flex-col items-center gap-2">
+    <button className="text-lg animate-bounce ease-out text-gray-400 flex flex-col items-center gap-2">
       Desliza hacia abajo
-      <div className="animate-bounce">
+      <div className="">
         <Arrowdown />
       </div>
     </button>
