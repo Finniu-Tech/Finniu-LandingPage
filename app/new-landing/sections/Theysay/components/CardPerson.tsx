ec6827c942f19002a13833c65099d299da5198a1
malsebasmal <antoniomaldonadoestudio@gmail.com>
2025-03-17 12:41:30 -0500

    change: the names of witnesses | add: new carousel for the section theysay with flyonUI | change: complete the const persons

---
+++ app/new-landing/sections/Theysay/components/CardPerson.tsx
@@ -0,0 +1,32 @@
+import Image, { StaticImageData } from "next/image"
+
+interface Props {
+  imgSrc: StaticImageData,
+  imgAlt: string,
+  title: string,
+  phrase: string,
+  name: string,
+  occupation: string
+}
+
+const CardPerson = ({imgSrc, imgAlt, title, phrase, name, occupation}:Props) => {
+  return (
+    <div className="max-w-sm h-full rounded-3xl border-[1px] border-solid bg-white border-whiteSecondary px-8 py-6 flex flex-col justify-between items-start">
+      <div className="w-full flex flex-col gap-2 items-start">
+        <Image src={imgSrc} alt={imgAlt} className="w-10" />
+        <h4 className="text-lg font-bold">
+          “{title}”
+        </h4>
+        <p>
+          {phrase}
+        </p>
+      </div>
+      <div className="w-full flex flex-col items-start border-t-[1px] border-solid border-whiteSecondary mt-4 pt-4">
+        <h3 className="text-lg font-bold">{name}</h3>
+        <h5>{occupation}</h5>
+      </div>
+    </div>
+  )
+}
+
+export default CardPerson
