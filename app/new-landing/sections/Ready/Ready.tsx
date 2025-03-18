b0033a0aa807792d8a881fd17eee1768a8221879
malsebasmal <antoniomaldonadoestudio@gmail.com>
2025-03-17 21:21:29 -0500

    add: complete the version mobile

--- app/new-landing/sections/Ready/Ready.tsx
+++ app/new-landing/sections/Ready/Ready.tsx
@@ -4,14 +4,14 @@ import Mackbook from "@/app/new-landing/sections/Ready/images/Macbook.webp"
 
 const Ready = () => {
   return (
-    <section className="py-40 pl-80 w-full bg-whitePrimary text-black flex justify-between items-center">
-      <div className="flex flex-col justify-center items-start gap-8">
+    <section className="py-10 md:py-40 px-8 pr-0 md:pl-80 w-full bg-whitePrimary text-black flex flex-col md:flex-row justify-between items-center">
+      <div className="flex flex-col pr-8 md:px-0 justify-center items-center md:items-start gap-2 md:gap-8">
         <h3 className="font-bold text-base">LISTOS PARA CRECER CONTIGO</h3>
-        <h3 className="w-[30rem] text-6xl leading-tight">Sin requisitos ocultos y sin comisiones</h3>
-        <h3 className="w-[30rem]">Regístrate de cualquier celular o computador y obtén más ganancias con tus ahorros. Genera un ingreso extra al mes</h3>
-        <ButtonLink href="#" text="Empieza ahora" className="mt-4 text-white py-4 px-6 rounded-full bg-purplePrincipal border-solid border-[1px] border-purplePrincipal hover:bg-white hover:text-purplePrincipal hover:border-solid hover:border-[1px] hover:border-purplePrincipal transition duration-150 ease"/>
+        <h3 className="w-full text-center md:text-start md:w-[30rem] text-5xl md:text-6xl leading-none md:leading-tight">Sin requisitos ocultos y sin comisiones</h3>
+        <h3 className="w-full text-center md:text-start md:w-[30rem]">Regístrate de cualquier celular o computador y obtén más ganancias con tus ahorros. Genera un ingreso extra al mes</h3>
+        <ButtonLink href="#" text="Empieza ahora" className="mt-2 md:mt-4 text-white py-4 px-6 rounded-full bg-purplePrincipal border-solid border-[1px] border-purplePrincipal hover:bg-white hover:text-purplePrincipal hover:border-solid hover:border-[1px] hover:border-purplePrincipal transition duration-150 ease"/>
       </div>
-      <div className="w-[60rem]">
+      <div className="w-full md:w-[60rem] mt-6 md:mt-0">
         <Image src={Mackbook} alt="Mackbook Finniu" className="w-full"/>
       </div>
     </section>
--- app/new-landing/sections/Whoweare/Whoweare.tsx
+++ app/new-landing/sections/Whoweare/Whoweare.tsx
@@ -10,22 +10,22 @@ import CountUp from "react-countup"
 
 const Whoweare = () => {
   return (
-    <section className="pb-40 bg-black text-white w-full flex flex-col justify-center items-center">
-      <section className="mt-40 flex flex-col justify-center items-center gap-6">
+    <section className="pb-10 md:pb-40 bg-black text-white w-full flex flex-col justify-center items-center">
+      <section className="mt-10 md:mt-40 flex flex-col justify-center items-center gap-6">
         <h3 className="text-lg text-gray-400">NOS RESPALDAN</h3>
-        <div className="flex justify-center items-center gap-20">
+        <div className="flex flex-col md:flex-row justify-center items-center gap-10 md:gap-20">
           <Image src={LogoInnovaul} alt="Logo de Innovaul" className="w-40" />
           <Image src={LogoIncubagraria} alt="Logo de Incubagraria" className="w-40" />
           <Image src={LogoEmprelatam} alt="Logo de Emprelatam" className="w-40" />
           <Image src={LogoInversionsimple} alt="Logo de Inversiones Simple" className="w-40" />
         </div>
       </section>
-      <section className="mt-28 px-80 w-full grid grid-cols-2 gap-20">
-        <div className="col-span-1 flex flex-col justify-center items-start gap-4">
-            <h3 className="text-7xl">¿Quiénes somos?</h3>
-            <p className="w-[35rem]">Somos una empresa Gestora de Fondos Privados con un producto de renta fija. En Finniu, impulsamos el crecimiento de las empresas de nuestro propio portafolio</p>  
+      <section className="mt-14 md:mt-28 px-8 sm:px-80 w-full grid grid-cols-1 md:grid-cols-2 gap-10 md:gap-20">
+        <div className="col-span-1 flex flex-col justify-center items-center md:items-start gap-2 md:gap-4 text-center md:text-start">
+          <h3 className="text-4xl md:text-7xl">¿Quiénes somos?</h3>
+          <p className="w-full md:w-[35rem]">Somos una empresa Gestora de Fondos Privados con un producto de renta fija. En Finniu, impulsamos el crecimiento de las empresas de nuestro propio portafolio</p>  
         </div>
-        <div className="col-span-1 grid grid-cols-2 gap-12">
+        <div className="col-span-1 grid grid-cols-1 md:grid-cols-2 text-center md:text-start gap-12">
           <div>
             <CountUp
               start={0}
@@ -33,13 +33,13 @@ const Whoweare = () => {
               duration={2.5}
               prefix="+"
               style={{
-                fontSize: "100px",
                 background: "linear-gradient(260.33deg, #0049FF -13.37%, #00BFD8 109.53%)",
                 WebkitBackgroundClip: "text",
                 WebkitTextFillColor: "transparent",
               }}
+              className="text-7xl md:text-[100px]"
             />
-            <h4>OPERACIONES</h4>
+            <h4 className="mt-2">OPERACIONES</h4>
           </div>
           <div>
             <CountUp
@@ -48,13 +48,13 @@ const Whoweare = () => {
               duration={2.5}
               prefix="+"
               style={{
-                fontSize: "100px",
                 background: "linear-gradient(265.11deg, #00FFB8 -5.84%, #005C90 111.58%)",
                 WebkitBackgroundClip: "text",
                 WebkitTextFillColor: "transparent",
               }}
+              className="text-7xl md:text-[100px]"
             />
-            <h4>INVERSIONISTAS</h4>
+            <h4 className="mt-2">INVERSIONISTAS</h4>
           </div>
           <div>
             <CountUp
@@ -65,12 +65,13 @@ const Whoweare = () => {
               prefix="+"
               suffix="M"
               style={{
-                fontSize: "100px",
                 background: "linear-gradient(261.81deg, #00FF91 -8.01%, #9A00FF 100%)",
                 WebkitBackgroundClip: "text",
                 WebkitTextFillColor: "transparent",
-              }}            />
-            <h4>SOLES INVERTIDOS</h4>
+              }}
+              className="text-7xl md:text-[100px]"
+            />
+            <h4 className="mt-2">SOLES INVERTIDOS</h4>
           </div>
         </div>
       </section>
