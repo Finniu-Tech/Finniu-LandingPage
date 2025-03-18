11fa111911b43d0692bb88e539686fd46259e779
malsebasmal <antoniomaldonadoestudio@gmail.com>
2025-03-17 21:39:33 -0500

    change: in the component investform

--- app/new-landing/sections/Invest/Invest.tsx
+++ app/new-landing/sections/Invest/Invest.tsx
@@ -8,10 +8,10 @@ import ArrowUpLeft from "@/app/new-landing/sections/Invest/images/icons/ArrowUpL
 const Invest = () => {
   return (
     <section
-      className="pt-10 md:pt-40 px-8 md:px-0 text-white w-full flex flex-col justify-center items-center"
+      className="pt-10 md:pt-40 pb-10 md:pb-0 px-8 md:px-0 text-white w-full flex flex-col justify-center items-center relative"
       style={{ backgroundImage: `url(${Background02.src})`, backgroundSize: 'cover', backgroundPosition: 'center' }}
     >
-      <section className="w-full flex flex-col justify-center items-center gap-10">
+      <section className="w-full flex flex-col justify-center items-center gap-6 md:gap-10">
         <div className="flex flex-col justify-center items-center gap-2 md:gap-8">
           <h3 className="font-bold">CALCULA TU PRESUPUESTO</h3>
           <h3 className="text-4xl md:text-6xl text-center">Proyecta tus Metas</h3>
@@ -23,8 +23,8 @@ const Invest = () => {
           <InvestmentForm />
         </div>
       </section>
-      <section className="pt-10 md:pt-40 px-0 md:px-80 w-full flex justify-between items-center relative">
-        <div className="w-[50%] md:w-[30rem] flex flex-col justify-center items-start gap-4 md:gap-6">
+      <section className="pt-10 md:pt-40 px-0 md:px-80 w-full flex justify-between items-center">
+        <div className="w-[75%] md:w-[30rem] flex flex-col justify-center items-start gap-4 md:gap-6">
           <h3 className="text-5xl md:text-6xl leading-none md:leading-tight">Invierte con confianza:</h3>
           <p>
             Cumplimos con los más estándares de seguridad
@@ -34,7 +34,7 @@ const Invest = () => {
           </p>
           <ButtonLinkIcon href="#" icon={<ArrowUpLeft />} text="Agendar videollamada" className="mt-4 flex gap-2 justify-center items-center text-white py-4 px-6 rounded-full bg-purplePrincipal hover:bg-white hover:text-purplePrincipal transition duration-150 ease"/>
         </div>
-        <div className="w-[15rem] md:w-[30rem] absolute md:static left-25 bottom-0">
+        <div className="w-[20rem] md:w-[30rem] absolute md:static -right-[10rem] -bottom-6">
           <Image src={LlamaCool} alt="Llama Cool" className="w-full" />
         </div>
       </section>
