f08488aa36fa1e526a602dcef34601c56dc21c05
malsebasmal <antoniomaldonadoestudio@gmail.com>
2025-03-19 10:47:47 -0500

    update: change the position for navbar icons in dropdown | update: direction of the footer section

---
+++ app/new-landing/sections/Navbar/images/icons/Portfolio.tsx
@@ -0,0 +1,11 @@
+const Portfolio = () => {
+  return (
+    <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M0.00890035 5.74246C-0.103446 4.85894 0.871273 4.25334 1.60722 4.74943L8.95389 9.70168L16.3974 4.74252C17.1368 4.2499 18.109 4.86262 17.99 5.74626L16.3402 18H1.56754L0.00890035 5.74246Z" fill="#3448FF" className="fill:#3448FF;fill:color(display-p3 0.2039 0.2824 1.0000);fill-opacity:1;"/>
+      <path fill-rule="evenodd" clip-rule="evenodd" d="M4.94727 4.6898L8.95223 7.47563L12.8999 4.76157L9.69165 0.407401C9.29141 -0.1358 8.50287 -0.1358 8.10262 0.4074L4.94727 4.6898Z" fill="#52B9F3" className="fill:#52B9F3;fill:color(display-p3 0.3220 0.7266 0.9542);fill-opacity:1;"/>
+    </svg>
+
+  )
+}
+
+export default Portfolio
